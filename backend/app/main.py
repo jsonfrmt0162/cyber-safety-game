f5f2beeac8f87282b8b5b74a44827b47efb7fa0c
jsnglvn <jasongalvan@gardenia.com.ph>
2025-12-30 12:36:50 +0800

    Add URL allowed from CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app = FastAPI(title="Cyber Safety Game API")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://cyber-safety-frontend.onrender.com", 
 ]
 
 app.add_middleware(
