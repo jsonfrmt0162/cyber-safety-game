470f8fe32638fffa71dae80de42e059a6d1250c3
jsnglvn <jasongalvan@gardenia.com.ph>
2025-12-30 12:10:21 +0800

    Make the URL for prod deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://cyber-safety-api.onrender.com";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
