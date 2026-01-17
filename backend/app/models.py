feeaee0fb3040907d9e17a641e56fc43284c881a
jsnglvn <jasongalvan@gardenia.com.ph>
2026-01-17 22:45:39 +0800

    declare string length of password

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)  
+    password = Column(String(255), nullable=False)
     is_admin = Column(Boolean, default=False) 
 
     birthday = Column(Date, nullable=False)
