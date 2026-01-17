3e24a3fe542b3d7021d20a431c1ca22cfe4c45f7
jsnglvn <jasongalvan@gardenia.com.ph>
2026-01-17 23:25:59 +0800

    Replace the api web

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
-//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://cyber-safety-api.onrender.com";
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://cyber-safety-api.onrender.com";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
